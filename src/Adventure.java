91e85f62e98c9b3c9dfe0749c9b951cdb6e722a8
tewan2657 <tewan2657@HRH-IC0027337.wrdsb.ca>
2017-11-16 10:12:52 -0500

    Starting game

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,3 +1,7 @@
+
+import java.io.FileReader;
+import java.net.URL;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,10 +13,35 @@
  */
 public class Adventure {
 
+    public void Coordinate(){
+        
+    }
+    
+    public void StartPos(){
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         //create a blank a file reader
+        FileReader file = null;
+        try {
+            //Fix this proper loading later
+            URL url = Adventure.class.getResource("Images.zip");
+
+            // creating the file reader
+            file = new FileReader(url.getFile());
+        } catch (Exception e) {
+            //handle any errors
+            //print out the red errors
+            e.printStackTrace();
+            //exit the program 
+            System.exit(0);
+        }
+        
+        
+        
     }
 }
